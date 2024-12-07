0d492dcc02e90ae53c43b68bd62f24171ec419d5
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-12-07 00:29:19 -0300

    .

---
+++ management_sys/src/novoFornecedor.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './novoFornecedor.css';
+import PlusIcon from "./IMGS/icon-plus.png"
+const novoFornecedor = () => {
+    return(
+        <div>
+            <div id="container">
+                <div id="header">
+                    <img src={PlusIcon} alt="" />
+                </div>
+                <div id="main">
+                    
+                </div>
+                <div id="footer">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default novoFornecedor;
