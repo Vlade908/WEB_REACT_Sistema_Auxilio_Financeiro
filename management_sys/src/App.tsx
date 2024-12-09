bb33d0671df982d7bf1dcbf7c6a5646363c8145d
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-12-09 16:24:20 -0300

    .
    .

--- management_sys/src/App.tsx
+++ management_sys/src/App.tsx
@@ -1,5 +1,5 @@
 import Login from "./Login"
-import { CadastroFornecedor } from "./components/CadastroFornecedor";
+
 import BtnAbrir from "./components/btn_Abrir_Modal";
 
 
@@ -11,8 +11,8 @@ function App() {
     <>
       <h1>Hello World</h1>
       <Login/>
-      <BtnAbrir text="Cadastrar Fornecedor">
-        <CadastroFornecedor text ='Cadastro Fornecedor'/>
+      <BtnAbrir tipo={1} >
+        
       </BtnAbrir>
       
       
--- management_sys/src/components/btn_Abrir_Modal.tsx
+++ management_sys/src/components/btn_Abrir_Modal.tsx
@@ -1,26 +1,80 @@
 import { ReactNode, useState } from "react";
 import Modal from "./modal";
 import './btn_Abrir_Modal.css'
+import { CadastroFornecedor } from "./CadastroFornecedor";
 
 interface ChildrenProps{
-    children: ReactNode;
+    children?: ReactNode;
     text?: string;
+    tipo?: number;
     
 }
 
 
 
 
-export default function BtnAbrir({text,children}:ChildrenProps) {
+export default function BtnAbrir({tipo,text}:ChildrenProps) {
     const [openModal,setOpenModal] = useState(false);
+    
+    const getButtonText = () => {
+        if (text) return text; // Se o texto for passado diretamente, usa-o
+        switch (tipo) {
+            case 1:
+                return "Cadastrar Fornecedor";
+            case 2:
+                return "Abrir Modal Tipo 2";
+            default:
+                return "Abrir Modal";
+        }
+    };
+
+
+    const renderModalContent = () => {
+        switch (tipo) {
+            case 1:
+                return (
+                <>
+                    <Modal isOpen={openModal}>
+                        <div>
+                            <CadastroFornecedor text ='Cadastro Fornecedor'/>
+                        </div>
+                    </Modal>
+                </>
+                    
+                );
+
+            case 2:
+                return (
+                    <Modal isOpen={openModal}>
+                        <div>
+                            
+                            <p>Este é o modal do tipo 2</p>
+                        </div>
+                    </Modal>
+                );
+
+            default:
+                return (
+                    <Modal isOpen={openModal}>
+                        <div>
+                            
+                            <p>Tipo de modal não especificado.</p>
+                        </div>
+                    </Modal>
+                );
+        }
+    };
+
     return(
+
+        
+
         <div >
-        <button id="btn" onClick={()=> setOpenModal(true)}>{text}</button>
-        <Modal isOpen={openModal}>
-        <div>
-            {children}
-        </div>
-        </Modal>
+        
+        <button id="btn" onClick={()=> setOpenModal(true)}>
+            {getButtonText()}
+        </button>
+        {openModal && renderModalContent()}
         </div>
 
 
