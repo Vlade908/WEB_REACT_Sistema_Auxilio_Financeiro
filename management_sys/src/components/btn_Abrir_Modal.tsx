4127c065c301f8dcf203f4d386d2011c8cc230ac
Enzo Alves <enzoalves.trabalho@gmail.com>
2024-12-09 13:31:09 -0300

    .

--- management_sys/src/components/btn_Abrir_Modal.tsx
+++ management_sys/src/components/btn_Abrir_Modal.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, useState } from "react";
 import Modal from "./modal";
-
+import './btn_Abrir_Modal.css'
 
 interface ChildrenProps{
     children: ReactNode;
@@ -9,8 +9,8 @@ interface ChildrenProps{
 export default function BtnAbrir({children}:ChildrenProps) {
     const [openModal,setOpenModal] = useState(false);
     return(
-        <div>
-        <button onClick={()=> setOpenModal(true)}>Abrir Modal</button>
+        <div >
+        <button id="btn" onClick={()=> setOpenModal(true)}>Abrir Modal</button>
         <Modal isOpen={openModal}>
         <div>
             {children}
